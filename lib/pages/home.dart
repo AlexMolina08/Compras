4191d750bde0a254bc2500a393253d8647e7c0dc
AlexMolina08 <alex98criado@gmail.com>
2020-03-08 18:31:28 +0100

    Añado Dialog para nueva compra

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart';
 *
 * */
 
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -19,6 +20,38 @@ class _HomeState extends State<Home> {
   TextEditingController _textController = TextEditingController();
 
   List<String> compras = ["Pan","tiramisu"];
+
+
+  _insertarCompra(BuildContext context){
+    return showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text("¿Qué quieres comprar?"),
+          content: TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              hintText: "Introduce producto",
+              suffixIcon: IconButton(
+                onPressed: () => _textController.clear(),
+                icon: Icon(Icons.clear),
+              )
+            ),
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              elevation: 5.0,
+              child: Icon(Icons.check),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            ),
+
+          ],
+        );
+      }
+    );
+  }
 
   @override
   void initState(){
@@ -33,47 +66,56 @@ class _HomeState extends State<Home> {
       appBar: PreferredSize(
         preferredSize: Size(double.infinity, 45), // 44 is the height
         child: AppBar(
-          backgroundColor: Colors.greenAccent,
+          backgroundColor: Colors.cyan,
           elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(34)
-            )
-          ),
           title: Text("Tu lista de la compra",
           style: TextStyle(
-            color: Colors.black,
+            letterSpacing: 2,
+            color: Colors.grey[200],
             fontWeight: FontWeight.normal,
             fontFamily: 'Lobster',
             fontSize: 30
           ),),
+
+          flexibleSpace: Container(
+
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: <Color>[
+                  Colors.orange,
+                  Colors.red
+                ],
+              ),
+            ),
+          ),
+
           centerTitle: true,
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            print("Has pulsado");
-          });
+          _insertarCompra(context);
         },
         elevation: 0,
         child: Icon(Icons.shopping_basket),
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.orange,
       ),
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
-
-      //CUERPO DEL SCAFFOLD
-
-      body: ListView.builder(
-        itemCount: compras.length,
-        itemBuilder: (BuildContext context , int index){
-          return ListTile(
-            title: Text('${compras[index]}'),
-          );
-        }
+      //CUERPO DEL SCAFFOLD.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: compras.length,
+          itemBuilder: (BuildContext context , int index){
+            return Card(child: ListTile(title: Text('${compras[index]}'))
+            );
+          }
+        ),
       )
 
 
